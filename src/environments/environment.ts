d9bd057fb8b3495d13293e11785d6c50862128b0
akosimarksalvador <markanthonybuhaysalvador@gmail.com>
2021-02-21 17:47:50 +0800

    currency code fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   paypal: {
     mode: 'sandbox',
     sandboxClientId: 'AW35MxsELLQji9DqLDeEK0wd7O1CjeBIVdlE1xs4p0VNQtaC9h-xzDY_lZFEm-4lmxQ-C6sT9uvCbiqr',
-    productionClientId: 'AVHs4otUqjovQtm-pkY9Mv_BLy_D5CRNCwZ0WuyhPyZuRhZHRNh7L2cV0M09kJI6TQidyW_bS4i61GiU',
+    productionClientId: '',
     currency: 'USD'
   }
 };
